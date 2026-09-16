e5eca4ef2c300d04d1e28c5d3e8046ccc3b7e89a
matheuspavao2 <yyyyeqze@proton.me>
2026-09-16 20:19:06 -0300

    Exercicio: inscricao em maratona (fluxo encadeado)

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
 let idade = Number(prompt("Qual é sua idade?"))
-let resultado = ["aprovado", "reprovado"]
+let resultado = ["reprovado", "aprovado"]
 
-// PRIMEIRA ETAPA
+// PRIMEIRA ETAPA & SEGUNDA ETAPA
 
-if (idade <= 16) {
-    alert(`Você foi ${resultado[1]} na 1° etapa!`)
-} else if (idade >= 60) {
+if (idade < 16) {
     alert(`Você foi ${resultado[0]} na 1° etapa!`)
-}
-// ATESTADO
+} else if (idade >=16 && idade < 60) {
+    alert(`Você foi ${resultado[1]} na 1° etapa!`)
 
-let atestado = confirm("Você tem atestado?")
-if (atestado) {
-    alert(`Você foi ${resultado[0]}!`)
-} else {
+} else if (idade >= 60) {
+    alert(`Você foi ${resultado[1]} na 1° etapa!`)
+
+let resposta = prompt("Você tem atestado? Digite 'Sim' ou 'Não'!")
+let atestado = resposta ? resposta.trim().toLowerCase(): "";
+
+if (atestado === "Sim" || atestado === "sim" || atestado === "Sim!" || atestado ==="sim!") {
     alert(`Você foi ${resultado[1]}!`)
+} else if (atestado === "Não" || atestado === "não" || atestado === "Não!" || atestado === "não") {
+    alert(`Você foi ${resultado[0]}! É necessário ter atestado para idosos com 60 anos ou mais.`)
+} else {
+    alert("Resposta inválida! Digite 'Sim' ou 'Não'!")
+}
+} else {
+    alert (`Por favor, informe sua idade!`)
 }
 
-// SEGUNDA ETAPA
+// TERCEIRA ETAPA
 
 let distância = Number(prompt(`Quantos quilômetros você percorreu?`))
 let tempo = Number(prompt("Por quantos minutos você percorreu?"))
-let pace = tempo / distância.toFixed(2)
+let pace = tempo / distância
 
 if (distância === 5 || distância === 15 || distância === 21 || distância === 42) {
 
- if (pace <= 5) {
-    alert(`Seu pace de ${pace}.0 foi Elite!`)
+if (pace <= 5) {
+    alert(`Seu pace de ${pace.toFixed(1)} foi Elite!`)
 } else if (pace <= 7.99) {
-    alert(`Seu pace de ${pace}.0 foi Competitivo!`)
+    alert(`Seu pace de ${pace.toFixed(1)} foi Competitivo!`)
 } else {
-    alert(`Seu pace ${pace}.0 foi Amador!`)
+    alert(`Seu pace ${pace.toFixed(1)}foi Amador!`) 
 }}
 else{
     alert ("Distância incorreta. As distâncias aceitas são: 5km, 15km, 21km e 42km!")
